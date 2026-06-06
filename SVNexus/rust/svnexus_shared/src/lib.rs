066df47fd1f390b838d512c8fc523e4f77669976
holdxen <zhousir429@gmail.com>
2026-06-06 12:45:35 +0800

    add deps

--- SVNexus/rust/svnexus_shared/src/lib.rs
+++ SVNexus/rust/svnexus_shared/src/lib.rs
@@ -1,14 +1,12 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct User {
+    pub username: String,
+    pub password: String,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[tarpc::service]
+pub trait TunnelService {
+    async fn authenticate(username: Option<String>) -> User;
 }
--- SVNexus/rust/svnexus_tunnel/src/main.rs
+++ SVNexus/rust/svnexus_tunnel/src/main.rs
@@ -1,3 +1,27 @@
+// svn+ssh://host/path	ssh -q -- host svnserve -t
+// svn+ssh://user@host/path	ssh -q -- user@host svnserve -t
+// svn+ssh://user@host:2222/path	ssh -q -- user@host:2222 svnserve -t
+// svn+ssh://user:pass@host/path	ssh -q -- user:pass@host svnserve -t
+
+use clap::Parser;
+
+#[derive(Parser)]
+struct Command {
+    #[arg(short)]
+    quiet: bool,
+
+    #[arg(long)]
+    rpc: String,
+
+    #[arg(trailing_var_arg = true, allow_hyphen_values = true)]
+    args: Vec<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cmd = Command::parse();
+
+    if cmd.args.len() < 1 {
+        tracing::info!("Invalid command call");
+        return;
+    }
 }
